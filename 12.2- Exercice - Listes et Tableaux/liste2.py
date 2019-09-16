cd99f09d815d055146055c746fbeb8bf34015e9f
PC-SORG\Windows <elv-samuel.dlvrr@eduge.ch>
2019-09-16 16:27:25 +0200

    Exercice 12.2 Listes et Tableaux

--- 12.2- Exercice - Listes et Tableaux/liste2.py
+++ 12.2- Exercice - Listes et Tableaux/liste2.py
@@ -2,3 +2,20 @@
 # Résultat attendu : Un affichage comme ceci : "La valeur maximum est :  x  et elle se trouve à l'indice [ n ][ m ]
 
 tab = [[4, 7, 3, 20, 42], [2, 4, 5, 7, 2], [23, 24, 15, 75, 23]]
+
+
+def searchmaxintab(table):
+    m = 0
+    for t in table:
+        for n in t:
+            if n > m:
+                m = n
+    return m
+
+def searchtabwithmax(table,m):
+    for t in table:
+       if m in t:
+           print("la valeur maximum est: ", m, " et elle se trouve à l'indice [",table.index(t),"][", t.index(m),"]")
+
+
+searchtabwithmax(tab, searchmaxintab(tab))
