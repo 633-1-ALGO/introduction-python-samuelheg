b008fa62a619dd4b9c834b7dc757f165e0aa0159
PC-SORG\Windows <elv-samuel.dlvrr@eduge.ch>
2019-09-16 16:36:38 +0200

    Exercice 13.1 Fonctions

--- 13.1- Exercice - Fonctions/fonction.py
+++ 13.1- Exercice - Fonctions/fonction.py
@@ -4,3 +4,23 @@ texte = "ceci est un texte que vous pouvez modifier mais gare aux caracteres spe
 tab_lettres = [
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
      'x', 'y', 'z', ' '], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+# Affichage du tableau mis à jour
+def frequencelettres(tab):
+    for lettre in tab[0]:
+        compte = texte.count(lettre)
+        tab[1][tab[0].index(lettre)] = compte
+    return tab
+
+
+print(frequencelettres(tab_lettres))
+
+# Affichage lettre par lettre
+def frequencelettres(tab):
+    for lettre in tab[0]:
+        compte = texte.count(lettre)
+        tab[1][tab[0].index(lettre)] = compte
+        print("le caractère ", lettre, "apparaît ", compte, " fois.")
+
+
+frequencelettres(tab_lettres)
